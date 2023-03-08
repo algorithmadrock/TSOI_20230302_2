d2cd05a97c3289c09c7c3af2a3333bdb37dc42e4
luiza <luizamfelix@gmail.com>
2023-03-08 12:24:13 -0300

    Atualização 08/03/2023

--- src/controller/KillerController.java
+++ src/controller/KillerController.java
@@ -1,5 +1,128 @@
 package controller;
 
-public class KillerController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class KillerController {
+	public KillerController() {
+		super();
+	}
+
+//	1)
+	private String os() {
+
+		String so = System.getProperty("os.name");
+//		String aq = System.getProperty("os.arch");
+//		String vr = System.getProperty("os.version");
+
+		return so;
+	}
+
+//	2)
+	public void listaProcessos() {
+		String SO = os();
+		if (SO.contains("Windows")) {
+			lP_Windows();
+		} else {
+			lP_Linux();
+		}
+	}
+
+	private void lP_Linux() {
+		try {
+			Process sproc = Runtime.getRuntime().exec("ps -ef");
+			InputStream fluxo = sproc.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+
+			String linha = buffer.readLine();
+
+			while (linha != null) {
+				System.out.println(linha);
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void lP_Windows() {
+		try {
+			Process sproc = Runtime.getRuntime().exec("TASKLIST /FO TABLE");
+			InputStream fluxo = sproc.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+
+			String linha = buffer.readLine();
+
+			while (linha != null) {
+				System.out.println(linha);
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+//	3)
+	public void mataPid(int pid) {
+		String SO = os();
+		if (SO.contains("Windows")) {
+			mP_Windows(pid);
+		} else {
+			mP_Linux(pid);
+		}
+	}
+
+	private void mP_Linux(int pid) {
+		try {
+			Process sproc = Runtime.getRuntime().exec("kill -9 " + pid);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void mP_Windows(int pid) {
+		try {
+			Process sproc = Runtime.getRuntime().exec("TASKKILL /PID " + pid);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+//	4)
+	public void mataNome(String nome) {
+		String SO = os();
+		if (SO.contains("Windows")) {
+			mN_Windows(nome);
+		} else {
+			mN_Linux(nome);
+		}
+	}
+
+	private void mN_Linux(String nome) {
+		try {
+			Process sproc = Runtime.getRuntime().exec("pkill -f " + nome);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void mN_Windows(String nome) {
+		try {
+			Process sproc = Runtime.getRuntime().exec("TASKKILL /IM " + nome);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,43 @@
+/*
+RESUMO      : Página principal com os comandos solicitados no exercício 01
+PROGRAMADORA: Luiza Felix
+DATA        : 02/03/2023
+ */
+
 package view;
 
+import javax.swing.JOptionPane;
+
+import controller.KillerController;
+
 public class Main {
+
+	public static void main(String[] args) {
+		KillerController killer = new KillerController();
+		int opcao = 0;
+
+		do {
+			opcao = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma configuração para ser acessada:\n 2) Lista de processos ativos\n 3) Matar um processo via PID\n 4)Matar um processo via NOME \n\n Para SAIR digite 0."));
+			switch (opcao) {
+			case 2:
+				killer.listaProcessos();
+				break;
+			case 3:
+				killer.mataPid(Integer.parseInt(JOptionPane.showInputDialog("Digite o PID do Processo.")));
+				break;
+			case 4:
+				killer.mataNome(JOptionPane.showInputDialog("Digite o NOME do Processo."));
+				break;
+
+			case 0:
+				System.out.println("Programa finalizado com sucesso! ");
+				break;
+			default:
+				System.out.println("!!! ERRO: Digite uma opção válida!!!");
+			}
+
+		} while (opcao != 0);
+
+	}
 
 }
